cdf16cce91884130d2fb1b020d02d2d4afe6901b
Ajinkya Bapat <ajinkyabapat1@gmail.com>
2020-06-28 18:44:06 +0530

    error handling done

--- app.js
+++ app.js
@@ -12,5 +12,19 @@ const orderRoute=require('./api/routes/orders');
 app.use('/products',productRoutes);
 
 app.use('/orders',orderRoute);
+app.use((req,res,next)=>{
+  const err= new Error('Page Not Found');
+  err.status=404;
+ next(err);
+})
+
+app.use((err,req,res,next)=>{
+  res.status(err.status||500);
+  res.json({
+   error:{
+     msg:err.message
+   }
+  })
+})
 
 module.exports=app;
